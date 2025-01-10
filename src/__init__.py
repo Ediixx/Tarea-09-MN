11c1031904b92b4f76f04a2743975640b608ae78
Ediixx <www.ediix_x@hotmail.com>
2025-01-10 12:32:03 -0500

    :memo: add all the exercise of Tarea 09

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from .linear_sist_methods import (
+    eliminacion_gaussiana,
+    eliminacion_gaussiana_pivoteo,
+    eliminacion_gaussiana_punto_flotante,
+    analizar_sistema,
+    Gauss_Jordan_punto_flotante,
+    descomposicion_LU,
+    resolver_LU,
+    matriz_aumentada,
+    separar_m_aumentada,
+)
